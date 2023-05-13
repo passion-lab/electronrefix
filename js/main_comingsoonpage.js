3059714e8f7024a2f485882fd2da01692f548601
passion-lab <connect.subhankar@protonmail.com>
2023-05-13 19:38:09 +0530

    Main Js file for the coming soon page is added

---
+++ js/main_comingsoonpage.js
@@ -0,0 +1,8 @@
+// JavaScript page for the  Coming Soon page
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        console.log("Loaded");
+        document.getElementById('loader').style.visibility = 'hidden';
+    }, 1000);
+});
